2c220628eed05c48f40ed84d8f7477a1b1f33514
Александр Зарубин <zarubin_14@list.ru>
2022-04-08 04:32:08 +0300

    Reading values from input

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
-const popup_button = document.getElementById("modal__button");
+const body = document.querySelector("body");
 const popup_window = document.querySelector(".popupF");
-const check_button = document.getElementById("input__check");
 const popup_content = document.querySelector(".popupF__content");
+const popup_button = document.getElementById("modal__button");
+const check_button = document.getElementById("input__check");
 const popup_close = document.querySelector(".popupF__close");
-const popup_city = document.getElementById(".input__city");
+const popup_buy = document.getElementById("popupF__buy");
+const popup_city = document.querySelector("#input__city");
+const popup_date = document.querySelector("#input__date");
+const popup_amount = document.querySelector("#input__amount");
+const popup_add_date = document.querySelector("#add__date");
+
+let Ticket = {
+    city: 0,
+    date: 0,
+    amount: 0,
+    addDate:0
+};
 
 popup_button.addEventListener("click",function (Event) {
     popup_window.style.visibility = "visible";
+    body.style["overflow-y"] = "hidden";
     Event.stopPropagation();
 })
-
 window.addEventListener("click", toHide);
 popup_close.addEventListener("click", toHide)
 popup_content.addEventListener("click", function(Event){
     Event.stopPropagation();
 })
-console.log(popup_button);
-console.log(popup_window);  
-console.log(popup_content);
+popup_buy.addEventListener("click", function(Event){
+    Ticket.city = popup_city.value;
+    Ticket.date = popup_date.value;
+    Ticket.amount = popup_amount.value;
+    Ticket.addDate = popup_add_date.value;
+    console.table(Ticket);
+    Event.preventDefault();
+})
 /*Visible of second date*/
 check_button.addEventListener("change", function (Event){
-    document.querySelector('.add__date').classList.toggle('hidden__div');
+    document.querySelector('.second__date').classList.toggle('hidden__div');
     Event.stopPropagation();
 })
 function toHide (){
     popup_window.style.visibility = "hidden";
+    body.style["overflow-y"] = "auto";
 }
